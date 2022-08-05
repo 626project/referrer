ad51fd1bc1df1329eea128b5ef1b2979ceca563b
kirill.s <kirill@project.com>
2022-08-05 23:42:12 +0300

    REF fix

--- public/tg/index.php
+++ public/tg/index.php
@@ -17,16 +17,22 @@ function request($method, $params = array()) {
 $data = json_decode(file_get_contents('php://input'), TRUE);
 file_put_contents(__DIR__ . '/message.txt', print_r($data, true));
 
-$data = isset($data['callback_query']) ? $data['callback_query'] : $data['message'];
-file_put_contents(__DIR__ . '/message2.txt', print_r($data, true));
+if (isset($data['callback_query'])) {
+    $data = $data['callback_query'];
+    $data = isset($data['data']) ? json_decode($data['data']) : [];
+    $message = mb_strtolower($data['action']);
+} else {
+    $data = $data['message'];
+    $message = mb_strtolower(($data['text'] ? $data['text'] : $data['data']));
+}
+file_put_contents(__DIR__ . '/message2.txt', print_r($message, true));
 
-$message = mb_strtolower(($data['text'] ? $data['text'] : $data['data']));
 
 switch ($message) {
     case '/start':
         $keyboard = array(
             array(
-                array('text'=>'Мне подходит вариант 1','callback_data'=>'{"action":"like","count":0,"text":":like:"}'),
+                array('text'=>'Мне подходит вариант 1','callback_data'=>'{"action":"variant_1","count":0,"text":":like:"}'),
                 array('text'=>'Мне подходит вариант 2','callback_data'=>'{"action":"joy","count":0,"text":":joy:"}'),
                 array('text'=>'Связаться с менеджером','callback_data'=>'{"action":"hushed","count":0,"text":":hushed:"}'),
                 array('text'=>'Частые вопросы','callback_data'=>'{"action":"cry","count":0,"text":":cry:"}'),
@@ -43,7 +49,7 @@ switch ($message) {
             'reply_markup' => json_encode(array('inline_keyboard' => $keyboard))
         ));
         break;
-    case 'Мне подходит вариант 1':
+    case 'variant_1':
         $keyboard = array(
             array(
                 array('text'=>'Может и вариант 2','callback_data'=>'{"action":"like","count":0,"text":":like:"}'),
