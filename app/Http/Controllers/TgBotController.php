68cd0c891df0716a2bdd16338ad39f5b36aa645b
kirill.s <kirill@project.com>
2022-08-05 20:17:27 +0300

    Revert "REF fix"
    This reverts commit a8e25cd1

--- app/Http/Controllers/TgBotController.php
+++ app/Http/Controllers/TgBotController.php
@@ -31,7 +31,7 @@ class TgBotController extends Controller
         // log input data
         info('input data: ' . print_r($data, 1));
 
-        $data = isset($data['callback_query']) ? $data['callback_query'] : $data['message'];
+        $data = $data['callback_query'] ? $data['callback_query'] : $data['message'];
 
         define('TOKEN', self::TOKEN);
 
