2a89b6cb2bca10c4b34f901ff0f3bc109337bcfa
kirill.s <kirill@project.com>
2022-08-05 23:31:42 +0300

    REF fix

--- public/tg/index.php
+++ public/tg/index.php
@@ -18,20 +18,63 @@ function request($method, $params = array()) {
 
     return json_decode(file_get_contents($url), JSON_OBJECT_AS_ARRAY);
 }
+$data = json_decode(file_get_contents('php://input'), TRUE);
+// log input data
+info('input data: ' . print_r($data, 1));
 
-$keyboard = array(
-    array(
-        array('text'=>':like:','callback_data'=>'{"action":"like","count":0,"text":":like:"}'),
-        array('text'=>':joy:','callback_data'=>'{"action":"joy","count":0,"text":":joy:"}'),
-        array('text'=>':hushed:','callback_data'=>'{"action":"hushed","count":0,"text":":hushed:"}'),
-        array('text'=>':cry:','callback_data'=>'{"action":"cry","count":0,"text":":cry:"}'),
-        array('text'=>':rage:','callback_data'=>'{"action":"rage","count":0,"text":":rage:"}')
-    )
-);
+$data = isset($data['callback_query']) ? $data['callback_query'] : $data['message'];
 
-request("sendMessage", array(
-    'chat_id' => CHAT_ID,
-    'text' => "hello world!",
-    'disable_web_page_preview' => false,
-    'reply_markup' => json_encode(array('inline_keyboard' => $keyboard))
-));
+define('TOKEN', self::TOKEN);
+
+$message = mb_strtolower(($data['text'] ? $data['text'] : $data['data']));
+
+switch ($message) {
+    case '/start':
+        $keyboard = array(
+            array(
+                array('text'=>'Мне подходит вариант 1','callback_data'=>'{"action":"like","count":0,"text":":like:"}'),
+                array('text'=>'Мне подходит вариант 2','callback_data'=>'{"action":"joy","count":0,"text":":joy:"}'),
+                array('text'=>'Связаться с менеджером','callback_data'=>'{"action":"hushed","count":0,"text":":hushed:"}'),
+                array('text'=>'Частые вопросы','callback_data'=>'{"action":"cry","count":0,"text":":cry:"}'),
+                array('text'=>'Отзывы','callback_data'=>'{"action":"rage","count":0,"text":":rage:"}'),
+                array('text'=>'Акция','callback_data'=>'{"action":"rage","count":0,"text":":rage:"}'),
+                array('text'=>'Договор оферта','callback_data'=>'{"action":"rage","count":0,"text":":rage:"}')
+            )
+        );
+
+        request("sendMessage", array(
+            'chat_id' => CHAT_ID,
+            'text' => "first message",
+            'disable_web_page_preview' => false,
+            'reply_markup' => json_encode(array('inline_keyboard' => $keyboard))
+        ));
+        break;
+    case 'Мне подходит вариант 1':
+        $keyboard = array(
+            array(
+                array('text'=>'Может и вариант 2','callback_data'=>'{"action":"like","count":0,"text":":like:"}'),
+            )
+        );
+
+        request("sendMessage", array(
+            'chat_id' => CHAT_ID,
+            'text' => "first message",
+            'disable_web_page_preview' => false,
+            'reply_markup' => json_encode(array('inline_keyboard' => $keyboard))
+        ));
+        break;
+    default:
+        $keyboard = array(
+            array(
+                array('text'=>'повторить','callback_data'=>'{"action":"like","count":0,"text":":like:"}'),
+            )
+        );
+
+        request("sendMessage", array(
+            'chat_id' => CHAT_ID,
+            'text' => "Команда не найдена. Повторите попытку",
+            'disable_web_page_preview' => false,
+            'reply_markup' => json_encode(array('inline_keyboard' => $keyboard))
+        ));
+}
+
