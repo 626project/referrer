2718543b25c111d897a6dc5950bccf1b6d94f301
kirill.s <kirill@project.com>
2022-08-31 01:23:47 +0300

    REF fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -86,7 +86,7 @@ class DashboardController extends Controller
     {
         $tg_users = TgUser::where(['link_id' => $link_id])
             ->whereIn('last_action', ['/start', 'variant 1', 'variant 2', 'variant 3', 'call manager', 'send a scan of your passport', 'need info about banks', 'i am ready', 'want a card', 'want a card. not resident', 'want a card. have inn', 'your question', 'participation in the action'])
-            ->distinct('last_action')
+            ->groupBy('last_action')
             ->orderBy('created_at', 'DESC')
             ->get();
 
