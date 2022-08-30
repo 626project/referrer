9474712f337403263e8047910ce088e6b8ac5e3c
kirill.s <kirill@project.com>
2022-08-31 02:31:56 +0300

    REF fix

--- app/Models/TgUserExportCollection.php
+++ app/Models/TgUserExportCollection.php
@@ -35,7 +35,8 @@ class TgUserExportCollection implements FromCollection
      */
     public function collection()
     {
-        $tg_users_request = TgUser::where(['link_id' => $this->link_id])
+        $tg_users_request = TgUser::select('id', 'tg_id', 'username', 'first_name', 'last_name', 'last_action', 'created_at')
+            ->where(['link_id' => $this->link_id])
             ->whereIn('last_action', ['/start', 'variant 1', 'variant 2', 'variant 3', 'call manager', 'send a scan of your passport', 'need info about banks', 'i am ready', 'want a card', 'want a card. not resident', 'want a card. have inn', 'your question', 'participation in the action']);
         if ($this->start_date) {
             $tg_users_request->where('created_at', '>=', $this->start_date);
