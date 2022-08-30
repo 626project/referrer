ca0f987460fb8b91b1ca7a525d85f666a6cef8d4
kirill.s <kirill@project.com>
2022-08-31 01:56:49 +0300

    REF fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,7 +9,7 @@ use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Redirector;
-use Excel;
+use Maatwebsite\Excel\Facades\Excel;
 
 class DashboardController extends Controller
 {
